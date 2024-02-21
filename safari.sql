2662d63dcf9e067eae6a9b36165af68872cb201e
Birindar <birindar@Birindars-MacBook-Air.local>
2024-02-21 18:54:32 +0000

    Attempted Extensions by myself

--- safari.sql
+++ safari.sql
@@ -48,6 +48,7 @@ INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Tony', 'Tiger', 59,
 INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Julien', 'Lemur', 12, 2);
 INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Gloria', 'Hippo', 23, 3);
 INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Momoto', 'Hippo', 25, 3);
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Maurice', 'Lemur', 59, 2);
 
 --Inserting values into assignments
 INSERT INTO assignments (staff_id, enclosure_id, day) VALUES (1, 1, 'Tuesday');
@@ -55,14 +56,30 @@ INSERT INTO assignments (staff_id, enclosure_id, day) VALUES (3, 2, 'Thursday');
 INSERT INTO assignments (staff_id, enclosure_id, day) VALUES (2, 3, 'Monday');
 
 -- MVP Queries
+
+--The names of the animals in a given enclosure
 SELECT name FROM animals WHERE enclosure_id = 2;
 
+--The names of the staff working in a given enclosure
 SELECT staff_id FROM assignments WHERE enclosure_id = 3;
 SELECT name FROM staff WHERE id = 2;
 
 --Extension Queries
 
+--The names of staff working in enclosures which are closed for maintenance
 SELECT id FROM enclosures WHERE closedForMaintenance = true;
 SELECT staff_id FROM assignments WHERE enclosure_id = 2 OR enclosure_id = 3;
 SELECT name FROM staff WHERE id = 2 OR id = 3;
 
+--The name of the enclosure where the oldest animal lives. If there are two animals who are the same age choose the first one alphabetically.
+ SELECT animals.name, animals.age, enclosures.name FROM enclosures INNER JOIN animals ON enclosures.id = animals.enclosure_id ORDER BY animals.age DESC, animals.name ASC;
+
+--The number of different animal types a given keeper has been assigned to work with.
+SELECT staff.name, COUNT (DISTINCT animals.type) FROM animals INNER JOIN assignments ON animals.enclosure_id = assignments.enclosure_id INNER JOIN staff ON assignments.staff_id = staff.id GROUP BY staff.name, animals.type;
+
+--The number of different keepers who have been assigned to work in a given enclosure
+SELECT enclosures.name, COUNT (DISTINCT staff.name) FROM enclosures INNER JOIN assignments ON enclosures.id= assignments.enclosure_id INNER JOIN staff ON assignments.staff_id = staff.id GROUP BY staff.name, enclosures.name;
+
+--The names of the other animals sharing an enclosure with a given animal (eg. find the names of all the animals sharing the big cat field with Tony)
+SELECT animals.id, animals.name, enclosures.name FROM enclosures INNER JOIN animals ON enclosures.id = animals.enclosure_id;
+SELECT * FROM animals WHERE enclosure_id = 2 AND animals.id != 2;
